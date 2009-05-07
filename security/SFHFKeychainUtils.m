22397f9bb08e6f38528bb53e1fd693468800041e
Buzz Andersen <buzz@scifihifi.com>
2009-05-07 13:45:07 -0400

    Undoing my attempted (ill-advised) preprocessor fix.

--- security/SFHFKeychainUtils.m
+++ security/SFHFKeychainUtils.m
@@ -32,7 +32,7 @@
 
 static NSString *SFHFKeychainUtilsErrorDomain = @"SFHFKeychainUtilsErrorDomain";
 
-#if TARGET_IPHONE_SIMULATOR & OBJC_API_VERSION < 2
+#if TARGET_IPHONE_SIMULATOR
 @interface SFHFKeychainUtils (PrivateMethods)
 + (SecKeychainItemRef) getKeychainItemReferenceForUsername: (NSString *) username andServiceName: (NSString *) serviceName error: (NSError **) error;
 @end
@@ -40,7 +40,7 @@ static NSString *SFHFKeychainUtilsErrorDomain = @"SFHFKeychainUtilsErrorDomain";
 
 @implementation SFHFKeychainUtils
 	
-#if TARGET_IPHONE_SIMULATOR & OBJC_API_VERSION < 2
+#if TARGET_IPHONE_SIMULATOR
 
 + (NSString *) getPasswordForUsername: (NSString *) username andServiceName: (NSString *) serviceName error: (NSError **) error {
 	if (!username || !serviceName) {
